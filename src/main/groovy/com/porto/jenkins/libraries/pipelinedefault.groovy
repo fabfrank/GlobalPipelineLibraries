c7325798a84db145db79d66c3a465500128f4d29
Fabricio Franklin dos Santos <fabfrank@gmail.com>
2018-05-20 20:41:21 -0300

    Update pipelinedefault.groovy

--- src/main/groovy/com/porto/jenkins/libraries/pipelinedefault.groovy
+++ src/main/groovy/com/porto/jenkins/libraries/pipelinedefault.groovy
@@ -3,114 +3,100 @@ def configfront = carregaProp()
 def call(Map pipelineParams) {
 
 pipeline {
-    agent {
-        label "master"
-    }
+	agent {
+		label propValue("agentlabel")
+	}
+
+	/*triggers {
+        cron('H 0 * * 1')
+    }*/
+
+	options {
+		buildDiscarder(logRotator(numToKeepStr: '30'))
+		gitLabConnection('GitLabCorp')
+		timeout(time: 60, unit:'MINUTES')
+		timestamps()
+	}
 	
-    options {
-        buildDiscarder(logRotator(numToKeepStr: '5'))
-        gitLabConnection('GitLabCorp')
-        timeout(time: 5, unit:'MINUTES')
-        timestamps()
-    }
-
-    tools {
-        jdk 'ibm-java-x86_64-71'
-        maven 'apache-maven-2.2.1'
-    }
-
-    environment {
+	tools {
+		jdk 'ibm-java-x86_64-71'
+		maven 'apache-maven-2.2.1'
+	}
+	
+	environment {
 		pom=''
-		CANAL_ROCKETCHAT="saconline"
-		EMAIL_REPORT=''
 		SONAR_TOKEN_USER="7e2f0c94bb0c4687dd8d90db536f170f3db355de"
 		ENCODING="UTF-8"
 		JAVA_SOURCE=1.7
 		JAVA_TARGET=1.7
 		MODULOEARNAME=''
-		deploy=''
 		SONAR_PROJECT_DESC="PR-2017-00049-SAC Online"
 		SLEEPQG=2
-		CHANGELOGS=''
-    }
+		AMBDEPLOY = ''
+		TIPOCOMPONENTE = ''
 
-    stages {
-		
-		stage("SCM branch Master") {
-			when {
-				branch 'master'
-			}
-			
-			steps {
-				gitlabCommitStatus(name: 'SCM branch Master') {
-					cleanWs()
-					
-					checkout scm
-					
-					git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
-				}
-			}
-			post {
-				failure {
-					script { env.FAILURE_STAGE = 'SCM branch Master' }
-				}
-				
-				success {
-				    createFileAppJson()
-				}
-			}
-		}
-		
-		stage("SCM Auto Merge") {
-			when {
-				expression { env.BRANCH_NAME != 'master' }
-			}
+		CLUSTERDEPLOY=propValue("clusterdeploy")
+		CREDENCRTC=propValue('credencialrtc')
+		WORKITEM=propValue('workitem')
+		COMPONENTE=propValue('nomeAplicacao')
+		CANAL_ROCKETCHAT=propValue('canalrocket')
+	}
+
+	stages {
+		stage("Checkout RTC") {
 
 			steps {
-				gitlabCommitStatus(name: 'SCM Auto Merge') {
-					
-					cleanWs()
-			        
-					checkout([
-					  $class: 'GitSCM',
-					  branches: scm.branches + [[name: "origin/${env.gitlabSourceBranch}"]],
-					  extensions: scm.extensions + [[
-						$class: 'CleanCheckout'],[
-						$class: 'UserIdentity', 
-							email: 'jenkinsCDS@portoseguro.com.br', name: 'JenkinsCDS'],[
-						$class: 'PreBuildMerge',
-						options: [
-						  fastForwardMode: 'FF',
-						  mergeRemote: 'origin',
-						  mergeStrategy: 'default',
-						  mergeTarget: 'master'
-						]
-					  ],[$class: 'PerBuildTag']],
-					  userRemoteConfigs: scm.userRemoteConfigs
-					])
+				cleanWs()
+
+				script {
+					if (EXCLUDECOMPONENTS.equals('NONE')) {
+						checkout([$class: 'RTCScm', avoidUsingToolkit: false, buildTool: 'jazz', buildType: [buildStream: "${STR}", clearLoadDirectory: true, 
+							generateChangelogWithGoodBuild: true, loadDirectory: '', value: 'buildStream'], credentialsId: "${CREDENCRTC}", overrideGlobal: true, 
+							serverURI: 'https://portoalm.portoseguro.com.br/ccm', timeout: 480])
+					} else {
+						checkout([$class: 'RTCScm', avoidUsingToolkit: false, buildTool: 'jazz', buildType: [buildStream: "${STR}", clearLoadDirectory: true, 
+							componentLoadConfig: 'excludeSomeComponents', componentsToExclude: "${EXCLUDECOMPONENTS}", customizedSnapshotName: '', 
+							generateChangelogWithGoodBuild: true, loadDirectory: '', loadPolicy: 'useComponentLoadConfig', processArea: '', value: 'buildStream'], 
+							credentialsId: "${CREDENCRTC}", overrideGlobal: true, serverURI: 'https://portoalm.portoseguro.com.br/ccm', timeout: 480])
+					}
 				}
 			}
 			
 			post {
 				failure {
-					script { env.FAILURE_STAGE = 'SCM Auto Merge' }
+					script { env.FAILURE_STAGE = 'Checkout RTC' }
 				}
-				
+
 				success {
-				    createFileAppJson()
+					script {
+						ARTHEFACT=getModuleParent().trim()
+					}
+				}
+				always {
+					script {
+						EMAIL_REPORT=env.EMAIL_REPORT + ", " + propValue('email')
+						CHANGELOGS = getChangeString()
+					}
 				}
 			}
 		}
 		
 		stage("Build Project") {
 			when {
-				expression { isChanged() }
+				expression { isChanged() || isTriggered()}
 			}
-			
+	
 			steps {
-				gitlabCommitStatus(name: 'Build Project') {
+				dir("${ARTHEFACT}") {
 					script {
-						sh 'mvn -Dmaven.test.failure.ignore clean -U install'
+						script {
+							MODULOEARNAME=getModulesEAR().trim()
+						}
+						if (env.SKIPTEST) {
+							sh "mvn clean -DskipTests -U ${BUILDPROFILE} install"
+						} else {
+							sh "mvn clean -Dmaven.test.failure.ignore -U ${BUILDPROFILE} install"
+						}
 					}
 				}
 			}
@@ -121,155 +107,81 @@ pipeline {
 				}
 
 				success {
-					script {
-						sh '''git update-index --assume-unchanged Jenkinsfile
-						      git update-index --assume-unchanged .bowerrc
-						      git update-index --assume-unchanged .gitignore
-						      git update-index --assume-unchanged .gitattributes'''
+					dir("${ARTHEFACT}") {
+						script {
+							TIPOCOMPONENTE = (MODULOEARNAME.equals('NONE') ? 'jar' : 'ear')
+							if (TIPOCOMPONENTE.equals('jar')) {
+								MODULOEARNAME = "${ARTHEFACT}"
+							}
+							
+							archiveArtifacts artifacts: "**/target/*.${TIPOCOMPONENTE}", fingerprint: true
+						}
 					}
 				}
-
+                changed {
+                    notifyEmail(env.EMAIL_REPORT, env.CHANGELOGS)
+                }
 				always {
-					script {
-						pom = readMavenPom file: 'pom.xml'
-						MODULOEARNAME = propValue('contexto')
-						CHANGELOGS = getChangeString()
-						EMAIL_REPORT = sh returnStdout: true, script: 'git log --pretty=\'%ae\' --since=\'3 minutes ago\''
+					dir("${ARTHEFACT}") {
+						script {
+							pom = readMavenPom file: 'pom.xml'
+						}
 					}
-				}
-			}
-		}
-		
-		stage("Push Auto Merge") {
-			when {
-				expression { env.BRANCH_NAME != 'master' && isChanged() }
-			}
-
-			steps {
-				gitlabCommitStatus(name: 'Push Auto Merge') {
-					
-					withEnv(["BRANCH_NAME=${BRANCH_NAME}"]) {
-						
-						sh '''v=`git remote -v`
-							 origin=$(echo $v | grep -E "^([^\\s]*)" | cut -d \' \' -f1-1)
-							 git push $origin HEAD:$BRANCH_NAME'''
-					}
-				}
-			}
-
-			post {
-				failure {
-					script { env.FAILURE_STAGE = 'Push Auto Merge' }
-				}
-			}
-		}
-		
-		stage("Deploy artifacts Nexus/Jenkins") {
-			when {
-				expression { isChanged() }
-			}
-
-			steps {
-				gitlabCommitStatus(name: 'Deploy Nexus') {
-					//nexusArtifactUploader artifacts: [[artifactId: "${MODULOEARNAME}", classifier: '', file: "target/${MODULOEARNAME}-${pom.version}.zip", type: 'zip']], credentialsId: 'NEXUS_ADMIN', groupId: "${pom.groupId}", nexusUrl: 'li2745:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshot', version: "${pom.version}"
-					archiveArtifacts allowEmptyArchive: true, artifacts: '**/target/*.zip', fingerprint: true, onlyIfSuccessful: true
-				}
-			}
-
-			post {
-				failure {
-					script { env.FAILURE_STAGE = 'Deploy artifacts Nexus/Jenkins' }
 				}
 			}
 		}
 		
 		stage('SonarQube analysis') {
 			when {
-				expression { isChanged() }
+				expression { isChanged() || isTriggered() }
 			}
 			
 			steps {
-				gitlabCommitStatus(name: 'SonarQube analysis') {
-					build job: 'sonarWork_Pipeline', 
-						parameters: [
-							string(name: 'PROJECTBASEDIR', value: "${WORKSPACE}"), 
-							string(name: 'CANAL_ROCKETCHAT', value: "${CANAL_ROCKETCHAT}"), 
-							string(name: 'SONAR_BRANCH', value: "${BRANCH_NAME}"), 
-							string(name: 'SONAR_PROJECT_DESC', value: "${SONAR_PROJECT_DESC}"), 
-							string(name: 'SONAR_TOKEN_USER', value: "${SONAR_TOKEN_USER}"), 
-							string(name: 'EMAIL_REPORT', value: "${EMAIL_REPORT}"), 
-							string(name: 'ENCODING', value: "${ENCODING}"), 
-							string(name: 'SLEEPQG', value: "${SLEEPQG}"), 
-							string(name: 'CHANGELOGS', value: "${CHANGELOGS}"), 
-							[$class: 'LabelParameterValue', name: 'node', label: "${NODE_NAME}"]], 
-							wait: false
-				}
+				build job: 'sonarWork_Pipeline', 
+					parameters: [
+						string(name: 'PROJECTBASEDIR', value: "${WORKSPACE}/${ARTHEFACT}"), 
+						string(name: 'CANAL_ROCKETCHAT', value: "${CANAL_ROCKETCHAT}"), 
+						string(name: 'SONAR_BRANCH', value: "${JOB_NAME}"), 
+						string(name: 'SONAR_PROJECT_DESC', value: "${SONAR_PROJECT_DESC}"), 
+						string(name: 'SONAR_TOKEN_USER', value: "${SONAR_TOKEN_USER}"), 
+						string(name: 'EMAIL_REPORT', value: "${EMAIL_REPORT}"), 
+						string(name: 'ENCODING', value: "${ENCODING}"), 
+						string(name: 'SLEEPQG', value: "${SLEEPQG}"), 
+						string(name: 'CHANGELOGS', value: "${CHANGELOGS}"), 
+						[$class: 'LabelParameterValue', name: 'node', label: "${NODE_NAME}"]], 
+						wait: false
 			}
-			
+
 			post {
 				failure {
 					script { env.FAILURE_STAGE = 'SonarQube analysis' }
 				}
-
-				changed {
-					notifySuccessful(env.CANAL_ROCKETCHAT, "${CHANGELOGS}" + '\n\n' + propValue('zelador') + ', aguardando sua aprovação para deploy no ambiente ' + propValue('deploy'), "${MODULOEARNAME}/${BRANCH_NAME}")
-				}
 			}
 		}
 		
-		stage('Deploy App Ambiente Dev') {
+		stage("Executar Deploy em ambiente de Teste?") {
 			when {
-				allOf {
-					expression { isChanged() }
-				}
+				expression { (isChanged() || isTriggered()) }
+				environment name: 'STAGEDEPLOY', value: 'true'
 			}
-			
-			agent {
-				label propValue('deployDEV')
-			}
-
 			steps {
-				gitlabCommitStatus(name: 'Deploy App') {
-					dir("${PATHTMPDEPLOY}") {
-						script {
-							//sh "sh download_artifact_nexus.sh -a ${pom.groupId.replace('.','/')}:${MODULOEARNAME}:${pom.version} -e zip  -n ${REPO_NEXUS} -o ${MODULOEARNAME}.zip"
-							sh "curl -sS ${BUILD_URL}artifact/target/${MODULOEARNAME}-${pom.version}.zip -o ${PATHAPPDEPLOY}/${MODULOEARNAME}.zip -v -R --location-trusted --fail"
-							sh returnStdout: false, script: "./ihs_deploy ${PATHIHS} ${MODULOEARNAME} '' '' '' '' '' ${PATHAPPDEPLOY}"
-						}
-					}
-				}
-			}
-
-			post {
-				failure {
-					script { env.FAILURE_STAGE = 'Deploy App' }
-				}
-				
-				always {
-					script {
-						sh "rm -rf ${PATHAPPDEPLOY}/${MODULOEARNAME}.zip"
-					}
-				}
-			}
-		}
-		
-		stage("Executar Deploy?") {
-			when {
-				expression { isChanged() && env.BRANCH_NAME == 'master'}
-			}
-
-			steps {
+			    
+                notifyEmail(propValue("emailZelador"), "Executar Deploy em ambiente de Teste?")
 			    script {
-			    	env.EXECDEPLOY = input message: 'Executar o deploy?',
-			        	parameters: [choice(name: 'Deploy de Aplicacao', choices: 'Não\nSim', description: 'Escolha "Sim" se você quer executar o deploy deste build no ambiente propValue("deploy")')]
+			    	env.EXECDEPLOY = input message: 'Executar Deploy em ambiente de Teste?',
+			        	parameters: [choice(name: 'Deploy de Aplicacao', choices: 'NÃ£o\nSim', description: 'Escolha "Sim" se vocÃª quer executar o deploy deste build no ambiente ' + propValue("deployTST"))], submitter: 'JazzUsers'
+			        
+			        if (EXECDEPLOY.equals('NÃ£o') ) {
+        	            currentBuild.result = "ABORTED"
+        	        }   
 			    }
 			}
 	    }
 		
-		stage('Deploy App') {
+		stage('Deploy App em ambiente de teste') {
 			when {
 				allOf {
-					expression { isChanged() }
+					expression { isChanged() || isTriggered() }
 					environment name: 'EXECDEPLOY', value: 'Sim'
 				}
 			}
@@ -277,66 +189,96 @@ pipeline {
 			agent {
 				label propValue('deployTST')
 			}
-
 			steps {
-				gitlabCommitStatus(name: 'Deploy App') {
-					dir("${PATHTMPDEPLOY}") {
-						script {
-							//sh "sh download_artifact_nexus.sh -a ${pom.groupId.replace('.','/')}:${MODULOEARNAME}:${pom.version} -e zip  -n ${REPO_NEXUS} -o ${MODULOEARNAME}.zip"
-							sh "curl -sS ${BUILD_URL}artifact/target/${MODULOEARNAME}-${pom.version}.zip -o ${PATHAPPDEPLOY}/${MODULOEARNAME}.zip -v -R --location-trusted --fail"
-							sh returnStdout: false, script: "./ihs_deploy ${PATHIHS} ${MODULOEARNAME} '' '' '' '' '' ${PATHAPPDEPLOY}"
-						}
+				script {
+
+					AMBDEPLOY = propValue('deployTST')
+
+					if (TIPOCOMPONENTE.equals('ear')) {
+					    sh "curl -sS ${BUILD_URL}artifact/${MODULOEARNAME}/target/${MODULOEARNAME}-${pom.version}.${TIPOCOMPONENTE} -o ${PATHAPPDEPLOY}/${MODULOEARNAME}.${TIPOCOMPONENTE} -v -R --location-trusted --fail"
+					    sh returnStdout: false, script: "${WRAPPER_WAS}/wrapper_was -lang jython -f ${WRAPPER_WAS}/installEar.py ${MODULOEARNAME} ${PATHAPPDEPLOY} ${MODULOEARNAME} ${AMBDEPLOY} ${CLUSTERDEPLOY} webserver1"
+					} else {
+					    sh "curl -sS ${BUILD_URL}artifact/target/${MODULOEARNAME}-${pom.version}.${TIPOCOMPONENTE} -o ${SHAREDLIBNAME}/${MODULOEARNAME}-${pom.version}.${TIPOCOMPONENTE} -v -R --location-trusted --fail"
 					}
 				}
 			}
-
 			post {
 				failure {
-					script { env.FAILURE_STAGE = 'Deploy App' }
+					script { env.FAILURE_STAGE = 'Deploy App em ambiente de teste' }
 				}
 				
 				always {
 					script {
-						sh "rm -rf ${PATHAPPDEPLOY}/${MODULOEARNAME}.zip"
+						sh "rm -rf ${PATHAPPDEPLOY}/${MODULOEARNAME}.${TIPOCOMPONENTE}"
 					}
 				}
 			}
 		}
-    }
-	
+	}
 	post {
+	    success {
+	        script {
+    	        if (!isChanged() && !isTriggered() ) {
+    	            currentBuild.result = "ABORTED"
+    	        }
+	        }
+	    }
 		failure {
-			notifyFailed(env.EMAIL_REPORT, env.CANAL_ROCKETCHAT, "${CHANGELOGS}", env.FAILURE_STAGE, "${MODULOEARNAME}/${BRANCH_NAME}")
+			notifyFailed(env.EMAIL_REPORT, env.CANAL_ROCKETCHAT, "${CHANGELOGS}", env.FAILURE_STAGE, "${JOB_NAME}")
 		}
 	}
 }
-}
 
 def notifyStarted(CANAL_ROCKETCHAT, CHANGES, MODULOEARNAME) {
-   rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: "${JENKINS_URL}static/ff676c77/images/headshot.png", channel: "${CANAL_ROCKETCHAT}", message: "STARTED: ${currentBuild.currentResult}", rawMessage: true 
+   rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: '${JENKINS_URL}static/ff676c77/images/headshot.png', channel: "${CANAL_ROCKETCHAT}", message: "STARTED: ${currentBuild.currentResult}", webhookToken: 'JyxPgTHx3EC6zNhab/d59qZ3AdGGNp47dihamry8wdzgZeohj4L9j5N6MTCNjKgoQb'
 }
 
 def notifySuccessful(CANAL_ROCKETCHAT, CHANGES, MODULOEARNAME) {
-    rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: "${JENKINS_URL}static/ff676c77/images/headshot.png", channel: "${CANAL_ROCKETCHAT}", message: "Build: ${currentBuild.currentResult}", rawMessage: true 
+    rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: '${JENKINS_URL}static/ff676c77/images/headshot.png', channel: "${CANAL_ROCKETCHAT}", message: "Build: ${currentBuild.currentResult}", webhookToken: 'JyxPgTHx3EC6zNhab/d59qZ3AdGGNp47dihamry8wdzgZeohj4L9j5N6MTCNjKgoQb'
 }
  
 def notifyFailed(EMAIL_REPORT, CANAL_ROCKETCHAT, CHANGES, STAGEERROR, MODULOEARNAME) {
-    rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: "${JENKINS_URL}static/ff676c77/images/headshot.png", channel: "${CANAL_ROCKETCHAT}", message: "Build: ${currentBuild.currentResult}", rawMessage: true
+    rocketSend attachments: [[audioUrl: '', authorIcon: '', authorName: '', color: 'green', imageUrl: '', messageLink: '', text: "${CHANGES}", thumbUrl: '', title: "${MODULOEARNAME}", titleLink: "${env.BUILD_URL}", titleLinkDownload: '', videoUrl: '']], avatar: '${JENKINS_URL}static/ff676c77/images/headshot.png', channel: "${CANAL_ROCKETCHAT}", message: "Build: ${currentBuild.currentResult}", webhookToken: 'JyxPgTHx3EC6zNhab/d59qZ3AdGGNp47dihamry8wdzgZeohj4L9j5N6MTCNjKgoQb'
+    
+    notifyEmail(EMAIL_REPORT, CHANGES)
+}
+
+def notifyEmail(EMAIL_REPORT, CHANGES) {
     
     emailext (
         to: "${EMAIL_REPORT}",
         subject: "${currentBuild.currentResult}: ${currentBuild.fullDisplayName}'",
-        body: """Stage error: ${STAGEERROR} <br><br><br><br><p>Check console output at "<a href="${env.BUILD_URL}">${currentBuild.currentResult}]</a>"</p><br>CHANGES: ${CHANGES}""",
+        body: """<p>Check console output at "<a href="${env.BUILD_URL}">${currentBuild.currentResult}]</a>"</p><br>CHANGES: ${CHANGES}""",
         recipientProviders: [[$class: 'DevelopersRecipientProvider']]
     )
 }
 
+def getModuleParent() {
+	sh returnStdout: true, script: '''PARENT=
+	for entry in *
+	do
+		if [ ! -f "$entry" ];then
+			valor=`find ./"${entry}" -type f -name \'pom.xml\' | grep -o "\\(.*\\)/" | sort -u | head -5`
+			if [ -n "$valor" ];then
+				PARENT="$entry"
+				break
+			fi
+		fi
+	done
+	echo $PARENT'''
+}
+
 def getModulesEAR() {
-	sh returnStdout: true, script: '''EAR=
+	sh returnStdout: true, script: '''EAR=NONE
 	for entry in *
 	do
 		if [ ! -f "$entry" ];then
 			valor=`find ./"${entry}" -type d -name \'application\' | grep -o "\\(.*\\)/" | sort -u | head -5`
+			
+			if [ ! -n "$valor" ];then
+				valor=`find ./"${entry}" -type f -name \'application.xml\' | grep -o "\\(.*\\)/" | sort -u | head -5`
+			fi
+			
 			if [ -n "$valor" ];then
 				EAR="$entry"
 				break
@@ -365,22 +307,17 @@ def getChangeString() {
     }
     return changeString
 }
-
 def isAPI(moduloear) {
     return moduloear.contains('API')
 }
 
-def createFileAppJson() {
-	writeFile encoding: 'UTF-8', file: "${WORKSPACE}/.bowerrc", text: '''{
-		"proxy" : " http://F0103972:fa09lusa@prxwg.portoseguro.brasil:3128",
-		"https-proxy" : "http://F0103972:fa09lusa@prxwg.portoseguro.brasil:3128",
-		"strict-ssl": false
-	} 
-'''
+@NonCPS
+def isTriggered() {
+  return (currentBuild.rawBuild.getCause(hudson.model.Cause$UpstreamCause) != null)
 }
 
 def isChanged() {
-    return !getChangeString().contains('No new changes')
+	return !getChangeString().contains('No new changes')
 }
 
 def carregaProp() {
@@ -391,4 +328,16 @@ def carregaProp() {
 
 def propValue(valor) {
 	return configfront[valor]
+}
+
+def isSegundaFeira() {
+    def retorno = false
+    def date = new Date()
+    sdf = new SimpleDateFormat("u")
+    
+    echo "Dia Semana: " + sdf.format(date)
+    if (sdf.format(date).equals('1')) {
+        retorno = true
+    }
+    return retorno
 }
